2e036c818bc28d3fc4c1957af537ddd3358bbb5e
mallalekitha <mallalekitha07@gmail.com>
2026-01-18 19:51:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-document.getElementById("welcomeBtn").addEventListener("click", function () {
-    alert("Welcome to my Student Profile Website!");
-});
-
-document.getElementById("detailsBtn").addEventListener("click", function () {
-    const details = document.getElementById("moreDetails");
-
-    if (details.classList.contains("hidden")) {
-        details.classList.remove("hidden");
-        this.textContent = "Hide Details";
-    } else {
-        details.classList.add("hidden");
-        this.textContent = "Show More Details";
-    }
-});
+function showDepartmentMessage() {
+    alert(
+        "Our Computer Science Department offers experienced faculty, modern labs, industry-oriented courses, and excellent career opportunities."
+    );
+}
